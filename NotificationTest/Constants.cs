464650f1299df310421ba6aadf3db35a409a12d0
kjun <keisoft.kjun@gmail.com>
2025-06-16 00:28:30 +0900

    ..

---
+++ NotificationTest/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace NotificationTest
+{
+    public class NotificationItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Message { get; set; } = "";
+        public string Type { get; set; } = "info";
+        public DateTime Timestamp { get; set; }
+    }
+
+    public class PopupItem
+    {
+        public string Title { get; set; } = "";
+        public string Message { get; set; } = "";
+        public string Type { get; set; } = "info";
+    }
+}
